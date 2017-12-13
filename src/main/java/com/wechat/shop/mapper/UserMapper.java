2cc5727ed57ce3bead354edc7c39bac752c85e36
lllZLXlll <zlx_ooo@126.com>
2017-12-13 11:58:45 +0800

    微信商城后台
    微信商城后台基本框架搭建

---
+++ src/main/java/com/wechat/shop/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.wechat.shop.mapper;
+
+import com.wechat.shop.entity.User;
+
+public interface UserMapper {
+    public User findUserInfo();
+}
---
+++ src/main/java/com/wechat/shop/service/UserService.java
@@ -0,0 +1,20 @@
+package com.wechat.shop.service;
+
+import com.wechat.shop.mapper.UserMapper;
+import com.wechat.shop.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User getUserInfo(){
+        User user=userMapper.findUserInfo();
+        //User user=null;
+        return user;
+    }
+
+}
